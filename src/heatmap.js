8885a839a7c7dd79f05c9720bacc35d82ed91904
Brock <brock.reece@croud.co.uk>
2017-12-09 18:28:07 +0000

    Add scrollmap directive and include a pausable stream

--- src/heatmap.js
+++ src/heatmap.js
@@ -1,5 +1,5 @@
 import h337 from 'heatmap.js';
-import { Observable } from 'rxjs';
+import { Observable, Subject } from 'rxjs';
 
 export default {
   install(Vue, options = {}) {
@@ -9,6 +9,79 @@ export default {
       e.value = value;
       return e;
     };
+
+    const getHeight = () => {
+      const body = document.body;
+      const html = document.documentElement;
+
+      return Math.max(body.scrollHeight, body.offsetHeight,
+              html.clientHeight, html.scrollHeight, html.offsetHeight);
+    };
+
+    const pauser = options.pauser || new Subject();
+
+    Vue.directive('scrollmap', {
+      inserted(el, binding) {
+        const height = getHeight();
+        const canvas = document.createElement('canvas');
+        const rounding = binding.arg || 50;
+        const show = binding.value || true;
+        const scroll = Observable.interval(2000);
+
+        let max = 0;
+
+        document.body.appendChild(canvas);
+        canvas.height = height;
+        canvas.style.height = `${height}px`;
+        canvas.width = window.outerWidth;
+        canvas.style.position = 'absolute';
+        canvas.style.left = 0;
+        canvas.style.top = 0;
+        canvas.style.width = '100%';
+        canvas.style.opacity = '0.8';
+        canvas.style.display = show;
+        canvas.id = 'scrollMapCanvas';
+        canvas.style.pointerEvents = 'none';
+
+        pauser.switchMap(paused => (paused ? Observable.never : scroll))
+          .map(() => window.scrollY)
+          .scan((acc, pos) => {
+            const rounded = Math.round(pos / rounding) * rounding;
+            let roundedWithHeight = Math.round((pos + window.innerHeight) / rounding) * rounding;
+
+            while (roundedWithHeight >= rounded) {
+              acc[roundedWithHeight] = acc[roundedWithHeight] || 0;
+              acc[roundedWithHeight] += 1;
+              max = Math.max(acc[roundedWithHeight], max);
+              roundedWithHeight -= rounding;
+            }
+
+            return acc;
+          }, { 0: 1 })
+          .subscribe((positions) => {
+            const c = document.getElementById('scrollMapCanvas');
+            c.height = height;
+            const ctx = c.getContext('2d');
+            const myGradient = ctx.createLinearGradient(0, 0, 0, c.height);
+
+            myGradient.addColorStop(0, 'hsla(0,50%,50%, 1)');
+            myGradient.addColorStop(1, 'hsla(0,50%,50%, 0)');
+
+            Object.keys(positions).forEach((key) => {
+              myGradient.addColorStop(Math.min(1, key / c.height), `hsla(0,50%,50%, ${positions[key] / max})`);
+            });
+
+            ctx.fillStyle = myGradient;
+            ctx.fillRect(0, 0, c.width, c.height);
+          });
+
+        pauser.next(false);
+      },
+
+      update(el, binding) {
+        document.getElementById('scrollMapCanvas').style.display = binding.value ? 'inherit' : 'none';
+      },
+    });
 
     Vue.directive('heatmap', {
       inserted(el, binding) {
@@ -30,7 +103,9 @@ export default {
           click.map(e => addValue(e, 10)),
         );
 
-        streams.debounceTime(10)
+        const pausable = pauser.switchMap(paused => (paused ? Observable.never : streams));
+
+        pausable.debounceTime(10)
           .map(e => ({
             x: e.layerX,
             y: e.layerY,
@@ -39,11 +114,11 @@ export default {
           .subscribe(e => heatmap.addData(e));
 
         if (options.afterAdd) {
-          streams.subscribe(options.afterAdd);
+          pausable.subscribe(options.afterAdd);
         }
 
         if (options.stream) {
-          streams.subscribe(e => options.stream.next(e));
+          pausable.subscribe(e => options.stream.next(e));
         }
 
         /* eslint-disable no-param-reassign */
